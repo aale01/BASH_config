50fcf5e7d616e573b6b69379b002e62bd3f5630b
Alessandro <148623060+aale01@users.noreply.github.com>
2026-06-24 19:45:40 +0200

    Fix color codes and update prompt format

--- prompt.sh
+++ prompt.sh
@@ -2,17 +2,18 @@
 
 # colori
 GREEN='\[\e[1;32m\]'
-BLUE='\[\e[1;34m\]'
-LIGHT_BLUE='\[\e[1;36m\]'
+BLUE='\[\e[1;36m\]'
+LIGHT_BLUE='\e[1;34m'
+RED='\e[1;31m'
 WHITE='\[\e[1;37m\]'
 RESET='\[\e[0m\]'
 
 git_branch() {
     local b
     b=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    [[ -n "$b" ]] && printf " (%s)" "$b"
+    [[ -n "$b" ]] && printf "${LIGHT_BLUE}git:(${RED}%s${LIGHT_BLUE}) " "$b"
 }
 
-PS1="${GREEN}\u@\h ${BLUE}\W${LIGHT_BLUE}\$(git_branch) ${RESET}➤ "
+PS1="${GREEN}\u@\h ${BLUE}\W \$(git_branch)${RESET}➤ "
 
 # in alternativa usare PROMPT_COMMAND oppure comandi come 'starship'
