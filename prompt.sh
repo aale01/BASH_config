559952f2984b468132725068a2e0b2b0f6388c8b
Alessandro Becucci <alebecu01@gmail.com>
2026-06-26 00:20:21 +0200

    fixed prompt

--- prompt.sh
+++ prompt.sh
@@ -1,19 +1,22 @@
 # ~/.bashrc.d/prompt.sh
 
 # colori
+# colori per PS1 (con \[ \])
 GREEN='\[\e[1;32m\]'
-BLUE='\[\e[1;36m\]'
-LIGHT_BLUE='\e[1;34m'
-RED='\e[1;31m'
-WHITE='\[\e[1;37m\]'
+LIGHT_BLUE='\[\e[1;36m\]'
 RESET='\[\e[0m\]'
+
+# colori per uso dentro funzioni/printf (senza \[ \])
+_BLUE='\001\e[1;34m\002'
+_RED='\001\e[1;31m\002'
+_RESET='\001\e[0m\002'
 
 git_branch() {
     local b
     b=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    [[ -n "$b" ]] && printf "${LIGHT_BLUE}git:(${RED}%s${LIGHT_BLUE}) " "$b"
+    [[ -n "$b" ]] && printf "${_BLUE}git:(${_RED}%s${_BLUE})${_RESET} " "$b"
 }
 
-PS1="${GREEN}\u@\h ${BLUE}\W \$(git_branch)${RESET}➤ "
+PS1="${GREEN}\u@\h ${LIGHT_BLUE}\W \$(git_branch)${RESET}➤ "
 
 # in alternativa usare PROMPT_COMMAND oppure comandi come 'starship'
