f1a2eddb82d81eb27de586d6ebf7472274819072
Alessandro <148623060+aale01@users.noreply.github.com>
2026-06-25 21:25:03 +0200

    Add Neovim binary path to PATH variable

--- env.sh
+++ env.sh
@@ -5,6 +5,7 @@ add_to_path() {
     [[ ":$PATH:" != *":$1:"* ]] && PATH="$1:$PATH"
 }
 
+add_to_path "/opt/nvim-linux-x86_64/bin"
 add_to_path "$HOME/.local/bin"
 add_to_path "$HOME/scripts"
 add_to_path "$HOME/.venv/bin"
